d87b8842d8a9140ab796d02c7b10a56168d5a8a1
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-04-09 20:15:41 +0900

    [Feat] Retrofit을 통한 네트워크 통신 기능 구현

--- app/app/src/main/java/test/map/goldddak/MainActivity.kt
+++ app/app/src/main/java/test/map/goldddak/MainActivity.kt
@@ -2,11 +2,72 @@ package test.map.goldddak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.*
+import test.map.goldddak.retrofit.Retrofit_Manager
+import java.lang.Exception
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        job = Job()
+
+
+        testleage("RANKED_SOLO_5x5", "GOLD", "I", 1)
+
+//        var int_data = 1;
+//
+//        while(true){
+//            if(int_data == 5){
+//                println("["+int_data+"]")
+//                break //while 무한루프 탈출
+//            }
+//            println("["+int_data+"]")
+//            int_data ++ //int 값 증가 실시
+//        }
+
 
     }
+
+
+    //소환사명을 가져오기 위한 함수
+    private fun testleage(queue: String, tire: String, division: String, page: Int) {
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.Main) {
+                    Retrofit_Manager.retrofitManager.EntireLeageCall(queue, tire, division, page)
+
+
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this@MainActivity, "${e.printStackTrace()}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun testsummoner() {
+
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.Main) {
+                    Retrofit_Manager.retrofitManager.SummonerCall("막나가기")
+
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this@MainActivity, "${e.printStackTrace()}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+
 }
--- app/app/src/main/java/test/map/goldddak/retrofit/Retrofit_Manager.kt
+++ app/app/src/main/java/test/map/goldddak/retrofit/Retrofit_Manager.kt
@@ -1,4 +1,98 @@
 package test.map.goldddak.retrofit
 
+import android.util.Log
+import android.widget.Button
+import retrofit2.Call
+import retrofit2.Response
+import test.map.goldddak.Riot_URL
+import test.map.goldddak.Riot_URL.API_KEY
+import test.map.goldddak.Riot_URL.RIOT_BASE_URL
+import test.map.goldddak.Riot_URL.TAG
+import test.map.goldddak.model.EntireLeageModel
+import test.map.goldddak.model.EntireLeageModelItem
+import test.map.goldddak.model.SummonerModel
+
 class Retrofit_Manager {
+
+    companion object {
+        val retrofitManager = Retrofit_Manager()
+    }
+
+    private val retrofitInterface: Retrofit_InterFace =
+        Retrofit_Client.getClient(RIOT_BASE_URL).create(Retrofit_InterFace::class.java)
+
+
+    suspend fun SummonerCall(summnoername: String) {
+        val call = retrofitInterface.summonerCall(
+            summoner = summnoername,
+            api_key = API_KEY
+        )
+
+        if (call.isSuccessful) {
+            Log.d(TAG, "SummonerCall: ${call.body()}")
+        }
+    }
+
+    suspend fun EntireLeageCall(queue: String, tire: String, division: String, page: Int) {
+
+//        val call = retrofitInterface.testleageCall(queue, tire, division, 1, API_KEY)
+
+        val call = retrofitInterface.leageCall(queue, tire, division, page, API_KEY)
+
+
+        var testInt = 1
+
+        while(call.isSuccessful){
+
+            if(call.body()==null){
+                break
+            }
+
+            val hi = retrofitInterface.leageCall(queue,tire,division, testInt, API_KEY)
+            Log.d(TAG, "무야호~~~: $hi")
+            testInt++
+        }
+
+
+
+//        if (call.isSuccessful) {
+//            Log.d(TAG, "EntireLeageCall: ${call.raw()}")
+//
+//        }
+    }
+
+    fun testsummonercall(summnoername: String) {
+        val call = retrofitInterface.testsummonerCall(summnoername, API_KEY)
+
+        call.enqueue(object : retrofit2.Callback<SummonerModel> {
+            override fun onResponse(call: Call<SummonerModel>, response: Response<SummonerModel>) {
+                val body = response.body()
+                Log.d(TAG, "onResponse: $body")
+            }
+
+            override fun onFailure(call: Call<SummonerModel>, t: Throwable) {
+                Log.d(TAG, "onFailure: $t")
+            }
+
+        })
+    }
+
+    fun testleageCall(queue: String, tire: String, division: String) {
+        val call = retrofitInterface.testleageCall(queue, tire, division, 1, API_KEY)
+
+        call.enqueue(object : retrofit2.Callback<EntireLeageModel> {
+            override fun onResponse(
+                call: Call<EntireLeageModel>,
+                response: Response<EntireLeageModel>,
+            ) {
+                Log.d(TAG, "onResponse: ${response.raw()}")
+            }
+
+            override fun onFailure(call: Call<EntireLeageModel>, t: Throwable) {
+                Log.d(TAG, "onFailure: $t")
+            }
+
+        })
+    }
+
 }
