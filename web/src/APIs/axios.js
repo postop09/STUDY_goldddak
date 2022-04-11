4bc58f389767262c45496fc103ec88d5a2257c1b
YUNSIK.CHO <postop09@naver.com>
2022-04-12 03:11:38 +0900

    [Feat] 소환사 데이터량 수정
    1. 티어별 3명에서 8명으로 수정
    2. 사용하지 않는 코드 제거(index.html)
    3. CORS 에러 발생

--- web/src/APIs/axios.js
+++ web/src/APIs/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://kr.api.riotgames.com',
   params: {
-    api_key: 'RGAPI-f532addb-a74f-4626-96fa-be93d239f67b',
+    api_key: 'RGAPI-b25b7a84-53d1-4a07-b95b-0e12d09d490c',
   },
 });
 
--- web/src/Components/Gold.jsx
+++ web/src/Components/Gold.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from '../APIs/axios';
+// import axios from 'axios';
 import requests from '../APIs/requests';
 
 export default function Gold() {
@@ -7,25 +8,25 @@ export default function Gold() {
   const [userData, setUserData] = useState([]);
   async function fetchUserData() {
     const resG1 = await axios.get(requests.fetchG1Data);
-    const summonersG1 = resG1.data.slice(0, 3);
+    const summonersG1 = resG1.data.slice(0, 8);
     console.log('골드1', summonersG1);
     summonersG1.map((summoner) => {
       setUserData((prev) => [...prev, summoner.summonerName]);
     })
     const resG2 = await axios.get(requests.fetchG2Data);
-    const summonersG2 = resG2.data.slice(0, 3);
+    const summonersG2 = resG2.data.slice(0, 8);
     console.log('골드2', summonersG2);
     summonersG2.map((summoner) => {
       setUserData((prev) => [...prev, summoner.summonerName]);
     })
     const resG3 = await axios.get(requests.fetchG3Data);
-    const summonersG3 = resG3.data.slice(0, 3);
+    const summonersG3 = resG3.data.slice(0, 8);
     console.log('골드3', summonersG3);
     summonersG3.map((summoner) => {
       setUserData((prev) => [...prev, summoner.summonerName]);
     })
     const resG4 = await axios.get(requests.fetchG4Data);
-    const summonersG4 = resG4.data.slice(0, 3);
+    const summonersG4 = resG4.data.slice(0, 8);
     console.log('골드4', summonersG4);
     summonersG4.map((summoner) => {
       setUserData((prev) => [...prev, summoner.summonerName]);
@@ -36,18 +37,20 @@ export default function Gold() {
   async function fetchPuuid() {
     console.log('소환사 이름 목록', userData);
     for (let i = 0; i < userData.length; i++) {
-      const res = await axios.get(`/lol/summoner/v4/summoners/by-name/${userData[i]}`);
-      // console.log(res.data);
+      const res = await axios.get(`/lol/summoner/v4/summoners/by-name/${userData[i]}`, {
+        mode: 'cors',
+      });
+      console.log(res.data);
       setPuuid((prev) => [...prev, res.data.puuid]);
     }
   }
   // matchId
   const [matchId, setMatchId] = useState([]);
   async function fetchMatchId() {
-    console.log('소환사 puuId', puuid);
+    // console.log('소환사 puuId', puuid);
     for (let i = 0; i < puuid.length; i++) {
       const res = await axios.get(`https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid[i]}/ids?start=0&count=2`);
-      // console.log(res.data);
+      console.log(res.data);
       setMatchId((prev) => [...prev, ...res.data])
     }
   }
