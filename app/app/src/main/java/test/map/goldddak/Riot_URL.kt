083d9f417f4f4f10c8aa7ef16d2f1b26149fc6ef
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-04-13 17:10:45 +0900

    [Feat] MATCHID 및 MATCHID의 BASE_URL 생성

--- app/app/src/main/java/test/map/goldddak/Riot_URL.kt
+++ app/app/src/main/java/test/map/goldddak/Riot_URL.kt
@@ -2,14 +2,17 @@ package test.map.goldddak
 
 object Riot_URL {
     const val RIOT_BASE_URL = "https://kr.api.riotgames.com"
+    const val RIOT_ASIA_URL = "https://asia.api.riotgames.com"
 
 
     const val LEAGE_URL = "/lol/league/v4/entries/{queue}/{tier}/{division}"
 
     const val SUMMNER_URL= "/lol/summoner/v4/summoners/by-name/{summonerName}"
 
-    const val MATCH_URL = "/lol/match/v5/matches/{matchId}"
+    const val MATCH_ID= "/lol/match/v5/matches/by-puuid/{puuid}/ids "
 
-    const val API_KEY = "RGAPI-eabb891c-10c1-4107-ad27-f19585f05177"
+    const val MATCH_INFO = "/lol/match/v5/matches/{matchId}"
+
+    const val API_KEY = "RGAPI-ea21173e-8466-4da8-9b18-64161f1291be"
     const val TAG:String = "로그"
 }
