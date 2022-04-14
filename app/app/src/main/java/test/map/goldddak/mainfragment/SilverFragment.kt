57d45748cfcba11b5c45d5b09db023ec8a1a13a2
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-04-14 16:02:47 +0900

    [Feat] Fragment activity 및 xml 추가

---
+++ app/app/src/main/java/test/map/goldddak/mainfragment/SilverFragment.kt
@@ -0,0 +1,20 @@
+package test.map.goldddak.mainfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import test.map.goldddak.databinding.SilverFragmentBinding
+
+class SilverFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = SilverFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
