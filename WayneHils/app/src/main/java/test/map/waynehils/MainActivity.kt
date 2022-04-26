e0f4cc28052b13cabf6d64ad60c894cf6a4da337
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-04-26 12:59:20 +0900

    first commit

---
+++ WayneHils/app/src/main/java/test/map/waynehils/MainActivity.kt
@@ -0,0 +1,11 @@
+package test.map.waynehils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
